5a2295a47c79bb1a5f6cfeb23bf23fe01e655da8
YashRaj1506 <yashraj504300@gmail.com>
2024-12-07 17:19:46 +0530

    Fixed #35414 -- Issue with AsyncClient ignoring default headers compared to synchronous Client.

--- tests/test_client/tests.py
+++ tests/test_client/tests.py
@@ -1250,6 +1250,20 @@ class AsyncClientTest(TestCase):
                 )
                 self.assertEqual(response.asgi_request.GET["example"], "data")
 
+    async def test_async_header_defaults(self):
+
+        request_factory_with_headers = AsyncRequestFactory(
+            **{
+                "Authorization": "Bearer faketoken",
+                "X-Another-Header": "some other value",
+            }
+        )
+        request = request_factory_with_headers.get("/somewhere/")
+        self.assertEqual(request.headers["authorization"], "Bearer faketoken")
+        self.assertIn("HTTP_AUTHORIZATION", request.META)
+        self.assertEqual(request.headers["x-another-header"], "some other value")
+        self.assertIn("HTTP_X_ANOTHER_HEADER", request.META)
+
 
 @override_settings(ROOT_URLCONF="test_client.urls")
 class AsyncRequestFactoryTest(SimpleTestCase):
